c841d0e3a09792c28073ccfb9070130ec7647ab1
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-08-29 18:40:40 +0300

    Added option to use saved token directly

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -1,5 +1,10 @@
 import { ipcRenderer } from 'electron';
+import loginDataStore from '../store/loginDataStore';
 
 export function startLoginWindow(): void {
     ipcRenderer.send('token-request');
+}
+
+export function loginWithToken(token: string): void {
+    loginDataStore.authToken = token;
 }
--- app/components/loginPage.tsx
+++ app/components/loginPage.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react';
 import { logIn } from '../actions/goproVideoList';
+import { Input } from '@material-ui/core';
+import { loginWithToken } from '../actions/login';
 
 const LoginPage = observer(
     (): JSX.Element => {
+        const [manualToken, setManualToken] = useState<string>('');
         return (
-            <div>
-                Not logged in!{' '}
-                <button onClick={(): void => logIn()}>Login</button>
-            </div>
+            <>
+                <div>
+                    Not logged in!
+                    <button onClick={(): void => logIn()}>
+                        Login with GoPro account
+                    </button>
+                </div>
+                <div>
+                    Or
+                    <br />
+                    Enter token manually:
+                    <Input
+                        value={manualToken}
+                        onChange={(e): void => setManualToken(e.target.value)}
+                    ></Input>
+                    <button onClick={(): void => loginWithToken(manualToken)}>
+                        Login with token
+                    </button>
+                </div>
+            </>
         );
     },
 );
